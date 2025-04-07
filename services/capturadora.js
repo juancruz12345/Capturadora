ae828d32a7b2b43dc344cd82d0932265032d1b25
juan_cruz <urbanjuancruz@gmail.com>
2025-04-07 16:17:28 -0300

    ffmpeg

--- services/capturadora.js
+++ services/capturadora.js
@@ -2,20 +2,25 @@ import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
 import { useRef } from 'react';
 
 export function useCapturadora(setShowControls, setLoading) {
+
+
+  ///DESARROLLO///////
   /*const ffmpeg = createFFmpeg({
     log: true,
     corePath: 'https://cdn.jsdelivr.net/npm/@ffmpeg/core@0.10.0/dist/ffmpeg-core.js',
     wasmPath: 'https://cdn.jsdelivr.net/npm/@ffmpeg/core@0.10.0/dist/ffmpeg-core.wasm',
-    ///threadSupport: false ///en desasrrollo debe estar en false para desabilitar multihilo
-     worker: false // 👈 evita usar SharedArrayBuffer
-  });*/
-
-  const ffmpeg = createFFmpeg({
-    log: true,
-    corePath: 'https://cdn.jsdelivr.net/npm/@ffmpeg/core@0.11.1/dist/ffmpeg-core.js',
-    wasmPath: 'https://cdn.jsdelivr.net/npm/@ffmpeg/core@0.11.1/dist/ffmpeg-core.wasm',
-    worker: false,
-  });
+    threadSupport: false ///en desasrrollo debe estar en false para desabilitar multihilo
+     
+  })*/
+ 
+    const ffmpeg = createFFmpeg({
+      log: true,
+      corePath: 'https://cdn.jsdelivr.net/npm/@ffmpeg/core@0.10.1/dist/ffmpeg-core.js',
+      wasmPath: 'https://cdn.jsdelivr.net/npm/@ffmpeg/core@0.10.1/dist/ffmpeg-core.wasm',
+      worker: false
+    });
+    
+    
   
 
 
